9a1ac03c2c3659b744cf5d4c9a79ffe8b2c7d07c
Petar Ivanov <peter.ivanov89@gmail.com>
2014-07-19 20:20:31 +0300

    inserts, erases, finds implemented

--- src/ordered_hash.h
+++ src/ordered_hash.h
@@ -24,6 +24,7 @@ class ordered_hash {
   typedef _Key                                key_type;
   typedef _Tp                                 mapped_type;
   typedef std::pair<key_type, mapped_type>    value_type;
+  typedef size_t                              size_type;
 
  private:
   typedef std::map<key_type, mapped_type>     ordered_t;
@@ -45,11 +46,27 @@ class ordered_hash {
   const_iterator begin() { return M.begin(); }
   const_iterator end() { return M.end(); }
       
-  void insert(const value_type &val) {
+  // inserts
+  pair<iterator, bool> insert(const value_type &val) {
     std::pair<iterator, bool> el = M.insert(val);
     H.insert( make_pair(val.first, el.first) );
+    return el;
   }
 
+  iterator insert (iterator position, const value_type& val) {
+    iterator it = M.insert(position, val);
+    H.insert( make_pair(val.first, it) );
+    return it;
+  }
+
+  template <class InputIterator>
+  void insert (InputIterator first, InputIterator last) {
+    for(InputIterator it = first; it != last; it++)
+      H.insert(*it);
+    return M.insert(first, last);
+  }
+
+  // finds
   iterator find(const key_type &key) {
     auto it = H.find(key);
     return it != H.end() ? it->second : M.end();
@@ -70,12 +87,25 @@ class ordered_hash {
   //  return const_cast<iterator &>( it );
   //}
 
-  void erase(const key_type &key) {
-    M.erase(key); // this O(logN) line is missing in ordered_hash_faster
-    H.erase(key);
+  // erases
+  void erase(iterator position) {
+    H.erase(*position);
+    return M.erase(position);
   }
 
-  int size() {
+  size_type erase(const key_type &key) {
+    H.erase(key);
+    return M.erase(key); // this O(logN) line is missing in ordered_hash_faster
+  }
+
+  void erase(iterator first, iterator last) {
+    for(iterator it = first; it != last; it++)
+      H.erase(*it);
+    M.erase(first, last);
+  }
+
+  // other
+  size_type size() const {
     //assert(M.size() == H.size());
     return H.size();
   }
